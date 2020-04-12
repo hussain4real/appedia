9f64ba0ae5919e0eb741cd05ccbdd45d788946be
Hussain Aminu <hussain_aminu@yahoo.com>
2020-04-12 17:56:56 +0100

    little bug fix

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -161,18 +161,18 @@
 @endsection
 
 <!-- all js here -->
-<script src="assets/js/vendor/jquery-1.12.0.min.js"></script>
-<script src="assets/js/popper.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
-<script src="assets/js/isotope.pkgd.min.js"></script>
-<script src="assets/js/imagesloaded.pkgd.min.js"></script>
-<script src="assets/js/jquery.counterup.min.js"></script>
-<script src="assets/js/waypoints.min.js"></script>
-<script src="assets/js/ajax-mail.js"></script>
-<script src="assets/js/owl.carousel.min.js"></script>
-<script src="assets/js/plugins.js"></script>
-<script src="assets/js/main.js"></script>
+{{-- <script src="assets/js/vendor/jquery-1.12.0.min.js"></script> --}}
+{{-- <script src="assets/js/popper.js"></script> --}}
+{{-- <script src="assets/js/bootstrap.min.js"></script> --}}
+{{-- <script src="assets/js/jquery.magnific-popup.min.js"></script> --}}
+{{-- <script src="assets/js/isotope.pkgd.min.js"></script> --}}
+{{-- <script src="assets/js/imagesloaded.pkgd.min.js"></script> --}}
+{{-- <script src="assets/js/jquery.counterup.min.js"></script> --}}
+{{-- <script src="assets/js/waypoints.min.js"></script> --}}
+{{-- <script src="assets/js/ajax-mail.js"></script> --}}
+{{-- <script src="assets/js/owl.carousel.min.js"></script> --}}
+{{-- <script src="assets/js/plugins.js"></script> --}}
+{{-- <script src="assets/js/main.js"></script> --}}
 {{--
 @section('extra-js')
 
